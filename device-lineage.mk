5a111521bd19b4f4b08bad9f4f08fd1ea6951e97
Ethan Chen <intervigil@gmail.com>
2018-12-01 13:53:00 -0800

    Declare EUICC permission
    Co-authored-by: Michael Bestas <mkbestas@gmail.com>
    Co-authored-by: razorloves <razorloves@gmail.com>
    Change-Id: I323dce8b848b44e45ed21002950059e58719113c

--- device-lineage.mk
+++ device-lineage.mk
@@ -5,3 +5,8 @@
 #
 
 DEVICE_PACKAGE_OVERLAYS += $(DEVICE_PATH)/overlay-lineage
+
+# EUICC
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.euicc.mep.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.mep.xml \
+    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
