5422afee60082fc4a0447b8fb6ab65c57378f9fc
Danny Lin <danny@kdrag0n.dev>
2021-11-19 09:17:04 -0800

    shusky: overlay: Settings: Enable Smooth Display setting
    All GS101 devices (raven and oriole) have peak refresh rates higher than
    60 Hz, so allow users to toggle high-refresh rate like on stock.
    
    Change-Id: I7fcbb905b61575de8c5e9d9a935463932551fb6a

--- device-lineage.mk
+++ device-lineage.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
+
 # ANGLE - Almost Native Graphics Layer Engine
 PRODUCT_PACKAGES += \
     ANGLE
