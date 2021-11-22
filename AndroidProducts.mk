8b71b1e1f4c80fc06a54c1873918d9f623836fc7
Chirayu Desai <chirayudesai1@gmail.com>
2021-11-22 21:22:09 +0530

    Add initial Lineage product makefiles
    Co-authored-by: Michael Bestas <mkbestas@gmail.com>
    Change-Id: I0781b6d4d74680ce7f0d860048871da7c2d43fb6

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -25,7 +25,9 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_shiba.mk \
     $(LOCAL_DIR)/aosp_shiba_fullmte.mk \
     $(LOCAL_DIR)/aosp_shiba_pgagnostic.mk \
-    $(LOCAL_DIR)/factory_shiba.mk
+    $(LOCAL_DIR)/factory_shiba.mk \
+    $(LOCAL_DIR)/lineage_husky.mk \
+    $(LOCAL_DIR)/lineage_shiba.mk
 
 COMMON_LUNCH_CHOICES := \
     aosp_ripcurrent-trunk_staging-userdebug \
---
+++ lineage_husky.mk
@@ -0,0 +1,33 @@
+#
+# SPDX-FileCopyrightText: 2021-2024 The LineageOS Project
+# SPDX-FileCopyrightText: 2021-2024 The Calyx Institute
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit some common stuff
+TARGET_DISABLE_EPPE := true
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit device configuration
+DEVICE_CODENAME := husky
+DEVICE_PATH := device/google/shusky
+$(call inherit-product, $(DEVICE_PATH)/aosp_$(DEVICE_CODENAME).mk)
+$(call inherit-product, device/google/zuma/lineage_common.mk)
+$(call inherit-product, $(DEVICE_PATH)/$(DEVICE_CODENAME)/device-lineage.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel 8 Pro
+PRODUCT_NAME := lineage_$(DEVICE_CODENAME)
+
+PRODUCT_SYSTEM_NAME := $(DEVICE_CODENAME)
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2992
+TARGET_SCREEN_WIDTH := 1344
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME) \
+    PRIVATE_BUILD_DESC="husky-user 14 AP2A.240905.003 12231197 release-keys"
+
+BUILD_FINGERPRINT := google/husky/husky:14/AP2A.240905.003/12231197:user/release-keys
