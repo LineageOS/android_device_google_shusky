e533e18f617575cbdf8d4612439f69df57a90caf
Robin Peng <robinpeng@google.com>
2022-10-30 15:38:55 +0000

    Initialize device folder
    from: e9419111f49c3305cbf35b4bfb709d986ece96a8
    with updated device path
    
    Bug: 248399843
    Change-Id: I409445ad16bb30d0ac9ccd18618df5826c963e62

---
+++ husky-sepolicy.mk
@@ -0,0 +1,3 @@
+# sepolicy exclusively for husky.
+BOARD_SEPOLICY_DIRS += device/google/shusky-sepolicy/husky
+
---
+++ shiba-sepolicy.mk
@@ -0,0 +1,3 @@
+# sepolicy exclusively for shiba.
+BOARD_SEPOLICY_DIRS += device/google/shusky-sepolicy/shiba
+
