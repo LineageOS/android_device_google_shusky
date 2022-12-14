7225af33493789ffe5f493d3623f9e0d5d21a52f
Darren Hsu <darrenhsu@google.com>
2022-12-14 17:11:13 +0800

    powerstats: update state residency and energy consumer for Display
    Bug: 262488433
    Bug: 262491621
    Test: dumpsys android.hardware.power.stats.IPowerStats/default
    Change-Id: I77ff227d3c50b135c3085f19f088f57a3b274cce
    Signed-off-by: Darren Hsu <darrenhsu@google.com>

--- powerstats/husky/service.cpp
+++ powerstats/husky/service.cpp
@@ -35,11 +35,12 @@ void addDisplay(std::shared_ptr<PowerStats> p) {
     // Add display residency stats
     std::vector<std::string> states = {
         "Off",
-        "LP: 1080x2340@30",
-        "On: 1080x2340@60",
-        "On: 1080x2340@90",
-        "HBM: 1080x2340@60",
-        "HBM: 1080x2340@90"};
+        "LP: 1344x2992@30",
+        "On: 1344x2992@10",
+        "On: 1344x2992@60",
+        "On: 1344x2992@120",
+        "HBM: 1344x2992@60",
+        "HBM: 1344x2992@120"};
 
     p->addStateResidencyDataProvider(std::make_unique<DisplayStateResidencyDataProvider>(
             "Display",
@@ -47,13 +48,11 @@ void addDisplay(std::shared_ptr<PowerStats> p) {
             states));
 
     // Add display energy consumer
-    p->addEnergyConsumer(PowerStatsEnergyConsumer::createMeterAndEntityConsumer(
-            p, EnergyConsumerType::DISPLAY, "display", {"PPVAR_VSYS_PWR_DISP"}, "Display",
-            {{"LP: 1080x2340@30", 1},
-             {"On: 1080x2340@60", 2},
-             {"On: 1080x2340@90", 3},
-             {"HBM: 1080x2340@60", 4},
-             {"HBM: 1080x2340@90", 5}}));
+    p->addEnergyConsumer(PowerStatsEnergyConsumer::createMeterConsumer(
+            p,
+            EnergyConsumerType::DISPLAY,
+            "DISPLAY",
+            {"VSYS_PWR_DISPLAY"}));
 }
 
 int main() {
