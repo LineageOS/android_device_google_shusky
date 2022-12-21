924330f04963bad51bd7acab655dec8bda817f20
Michael Bestas <mkbestas@lineageos.org>
2022-12-22 01:02:18 +0200

    shusky: overlay: Configure aux cameras for Aperture
    Change-Id: I3d1c93057b5c2a1ba09c06a945d538d2043f30fd

--- husky/device-lineage.mk
+++ husky/device-lineage.mk
@@ -4,6 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+DEVICE_PACKAGE_OVERLAYS += device/google/shusky/husky/overlay-lineage
+
 TARGET_VENDOR_PROP += device/google/shusky/husky/vendor.prop
 
 $(call inherit-product, device/google/shusky/device-lineage.mk)
--- shiba/device-lineage.mk
+++ shiba/device-lineage.mk
@@ -4,6 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+DEVICE_PACKAGE_OVERLAYS += device/google/shusky/shiba/overlay-lineage
+
 TARGET_VENDOR_PROP += device/google/shusky/shiba/vendor.prop
 
 $(call inherit-product, device/google/shusky/device-lineage.mk)
