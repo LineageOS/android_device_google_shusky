862356a8443598f0ea9d2b92850aec586d4fd0d4
Tai Kuo <taikuo@google.com>
2023-01-05 17:08:44 +0800

    vibrator: remove vibrator
    Bug: 264625320
    Test: HAL does not start.
    Change-Id: I8fd1d72e419a8af3000fbe3d593229a7a868e589

--- device-shiba.mk
+++ device-shiba.mk
@@ -26,7 +26,6 @@ DEVICE_PACKAGE_OVERLAYS += device/google/shusky/shiba/overlay
 
 include device/google/shusky/audio/shiba/audio-tables.mk
 include device/google/zuma/device-shipping-common.mk
-include device/google/shusky/vibrator/cs40l26/device-shusky.mk
 include device/google/gs-common/bcmbt/bluetooth.mk
 include device/google/gs-common/touch/gti/gti.mk
 
