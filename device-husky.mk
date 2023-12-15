04533178967884e2eb76a4b3920e9cc48a8c593c
Ankur Bakshi <ankurbakshi@google.com>
2023-12-15 21:21:49 +0000

    Update husky SVN to 15
    Bug: 313797924
    Change-Id: Ib2b43f53edb5f2611e2acd5f529efbf9e51f567d
    (cherry picked from commit e6344e5d668b2a9963f01d43bfc3a6ec5645265f)
    Merged-In: Ib2b43f53edb5f2611e2acd5f529efbf9e51f567d

--- device-husky.mk
+++ device-husky.mk
@@ -352,7 +352,7 @@ PRODUCT_VENDOR_PROPERTIES += \
 
 # Increment the SVN for any official public releases
 PRODUCT_VENDOR_PROPERTIES += \
-    ro.vendor.build.svn=14
+    ro.vendor.build.svn=15
 
 # WLC userdebug specific
 ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
