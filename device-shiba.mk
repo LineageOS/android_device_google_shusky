e27391c550e2a48e4d1c44b9cd1bb926656a18b7
Ankur Bakshi <ankurbakshi@google.com>
2024-01-28 00:46:42 +0000

    Update shiba SVN to 18
    Bug: 313797924
    Change-Id: Ie98ca590bf0e2680cdce7ee0a62208c876879b51

--- device-shiba.mk
+++ device-shiba.mk
@@ -374,7 +374,7 @@ PRODUCT_VENDOR_PROPERTIES += \
 
 # Increment the SVN for any official public releases
 PRODUCT_VENDOR_PROPERTIES += \
-    ro.vendor.build.svn=17
+    ro.vendor.build.svn=18
 
 # P23 Devices no longer need rlsservice
 PRODUCT_VENDOR_PROPERTIES += \
