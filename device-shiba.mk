49b2e4ca46286b5324f0712057ea8c3b6ef2eaa1
Ankur Bakshi <ankurbakshi@google.com>
2024-02-15 02:23:11 +0000

    Update shiba SVN to 22
    Bug: 325156176
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:99a2ca4be8cb6730e0ae565be9d7cb86149db7ae)
    Merged-In: I10d48ca72b810cac33093cbb2e9364383d916075
    Change-Id: I10d48ca72b810cac33093cbb2e9364383d916075

--- device-shiba.mk
+++ device-shiba.mk
@@ -374,7 +374,7 @@ PRODUCT_VENDOR_PROPERTIES += \
 
 # Increment the SVN for any official public releases
 PRODUCT_VENDOR_PROPERTIES += \
-    ro.vendor.build.svn=21
+    ro.vendor.build.svn=22
 
 # P23 Devices no longer need rlsservice
 PRODUCT_VENDOR_PROPERTIES += \
