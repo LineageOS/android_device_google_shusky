589cd27c8637c15536d59159e29ea6dc82ac4a93
Ankur Bakshi <ankurbakshi@google.com>
2024-02-15 02:25:50 +0000

    Update husky SVN to 21
    Bug: 325156176
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:edcc8052bef05250ebf493dd743279f96b799613)
    Merged-In: Ib5a0543902942460fb6818c3d8a540b4d9f5d5de
    Change-Id: Ib5a0543902942460fb6818c3d8a540b4d9f5d5de

--- device-husky.mk
+++ device-husky.mk
@@ -400,7 +400,7 @@ PRODUCT_VENDOR_PROPERTIES += \
 
 # Increment the SVN for any official public releases
 PRODUCT_VENDOR_PROPERTIES += \
-    ro.vendor.build.svn=20
+    ro.vendor.build.svn=21
 
 # WLC userdebug specific
 ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
