8a517f4a68d27bf2e13f6a966df9fc7bcbe9a5ce
Limon Mia <limonmia@google.com>
2024-03-05 07:43:44 +0000

    BT: Set system property for Bluetooth Auto On for P23
    Set bluetooth.server.automatic_turn_on true for Bluetooth Auto On
    feature.
    The feature will be enabled with both system property and flag set.
    
    Bug: 328168961
    Test: check system property value
    Change-Id: I0c6785fc7daa51b728872892c83ad20babe3253b

--- device-husky.mk
+++ device-husky.mk
@@ -118,6 +118,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.bluetooth.a2dp_offload.disabled=false \
     persist.bluetooth.a2dp_offload.cap=sbc-aac-aptx-aptxhd-ldac-opus
 
+# Enable Bluetooth AutoOn feature
+PRODUCT_PRODUCT_PROPERTIES += \
+    bluetooth.server.automatic_turn_on=true
+
 # Bluetooth Tx power caps
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/bluetooth/bluetooth_power_limits_husky.csv:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_power_limits.csv \
