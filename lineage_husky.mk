7ef593788888b9b56772c9cd39378dba3ae5c440
Michael Bestas <mkbestas@lineageos.org>
2024-03-10 22:10:56 +0200

    Update fingerprint/build description from AP1A.240305.019.A1
    Change-Id: I025131f25d20484d0c0ce682a9a21f2707b1a506

--- lineage_husky.mk
+++ lineage_husky.mk
@@ -25,8 +25,8 @@ TARGET_SCREEN_WIDTH := 1344
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=husky \
-    PRIVATE_BUILD_DESC="husky-user 14 UQ1A.240205.004 11269751 release-keys"
+    PRIVATE_BUILD_DESC="husky-user 14 AP1A.240305.019.A1 11445699 release-keys"
 
-BUILD_FINGERPRINT := google/husky/husky:14/UQ1A.240205.004/11269751:user/release-keys
+BUILD_FINGERPRINT := google/husky/husky:14/AP1A.240305.019.A1/11445699:user/release-keys
 
 $(call inherit-product, vendor/google/husky/husky-vendor.mk)
