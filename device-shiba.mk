1c184ad08814b365c4bd96d0c15f50785a6b5953
Ankur Bakshi <ankurbakshi@google.com>
2024-04-03 23:25:15 +0000

    Update shiba SVN to 25
    Bug: 330382848
    Change-Id: I5a6512a749937e4b1fb845253962b10268c77913

--- device-shiba.mk
+++ device-shiba.mk
@@ -379,7 +379,7 @@ PRODUCT_VENDOR_PROPERTIES += \
 
 # Increment the SVN for any official public releases
 PRODUCT_VENDOR_PROPERTIES += \
-    ro.vendor.build.svn=24
+    ro.vendor.build.svn=25
 
 # P23 Devices no longer need rlsservice
 PRODUCT_VENDOR_PROPERTIES += \
