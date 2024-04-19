ea3a914a038eb5651d83cd5d9e24e4e152d40de5
Lokesh Kumar Goel <lokeshgoel@google.com>
2024-04-19 20:59:35 +0000

    shusky: Add build_flags based properties for SMR/EMR build
    Flag: build.RELEASE_IS_SMR
    Flag: build.RELEASE_IS_EMR
    Flag: build.RELEASE_BASE_OS_HUSKY
    Flag: build.RELEASE_BASE_OS_SHIBA
    Bug: 155105803
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:28a1b27468d204366837ce4ce8e2e5559ac6bf2a)
    Merged-In: Ibec4f0c908089ca4304e59e9d37529fa2d4fa40d
    Change-Id: Ibec4f0c908089ca4304e59e9d37529fa2d4fa40d

--- device-husky.mk
+++ device-husky.mk
@@ -405,6 +405,24 @@ endif
 PRODUCT_VENDOR_PROPERTIES += \
     ro.vendor.build.svn=$(TARGET_SVN)
 
+# Set device family property for SMR
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.device_family=HK3SB3AK3
+
+# Set build properties for SMR builds
+ifeq ($(RELEASE_IS_SMR), true)
+    ifneq (,$(RELEASE_BASE_OS_HUSKY))
+        PRODUCT_BASE_OS := $(RELEASE_BASE_OS_HUSKY)
+    endif
+endif
+
+# Set build properties for EMR builds
+ifeq ($(RELEASE_IS_EMR), true)
+    ifneq (,$(RELEASE_BASE_OS_HUSKY))
+        PRODUCT_PROPERTY_OVERRIDES += \
+        ro.build.version.emergency_base_os=$(RELEASE_BASE_OS_HUSKY)
+    endif
+endif
 # WLC userdebug specific
 ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
     PRODUCT_COPY_FILES += \
