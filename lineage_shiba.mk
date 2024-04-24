71c89825b6c3c9e169288980d9bd357404e923d8
Michael Bestas <mkbestas@lineageos.org>
2024-04-24 04:24:41 +0300

    Update fingerprint/build description from AP1A.240405.002.B1
    Change-Id: Ia65eeb044e38387b200e8be26474b890915ae48d

--- lineage_shiba.mk
+++ lineage_shiba.mk
@@ -25,8 +25,8 @@ TARGET_SCREEN_WIDTH := 1080
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=shiba \
-    PRIVATE_BUILD_DESC="shiba-user 14 AP1A.240405.002 11480754 release-keys"
+    PRIVATE_BUILD_DESC="shiba-user 14 AP1A.240405.002.B1 11677284 release-keys"
 
-BUILD_FINGERPRINT := google/shiba/shiba:14/AP1A.240405.002/11480754:user/release-keys
+BUILD_FINGERPRINT := google/shiba/shiba:14/AP1A.240405.002.B1/11677284:user/release-keys
 
 $(call inherit-product, vendor/google/shiba/shiba-vendor.mk)
