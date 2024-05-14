fab6a485f9ed2e3b4fc1fcf51d951443a6ac095f
Juan Yescas <jyescas@google.com>
2024-05-14 23:58:02 +0000

    16kb: Use RELEASE_KERNEL_SHIBA_DIR for shiba targets
    Test: Built locally
    Bug: 333734531
    Bug: 308663811
    Change-Id: I6190bd55ac2e7007df4f6cab5b64a0666e0a0314

--- device-shiba.mk
+++ device-shiba.mk
@@ -28,7 +28,7 @@ DEVICE_PAGE_AGNOSTIC ?= $(if $(filter %_pgagnostic,$(TARGET_PRODUCT)),true,false
 
 ifeq ($(DEVICE_PAGE_AGNOSTIC),true)
 TARGET_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
-TARGET_KERNEL_DIR := $(RELEASE_KERNEL_HUSKY_DIR)/16kb
+TARGET_KERNEL_DIR := $(RELEASE_KERNEL_SHIBA_DIR)/16kb
 endif
 
 $(call inherit-product-if-exists, vendor/google_devices/shusky/prebuilts/device-vendor-shiba.mk)
