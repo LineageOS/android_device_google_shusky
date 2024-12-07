0b1ff354b5a5e76f8aca2b400eeeca1fc64db659
Michael Bestas <mkbestas@gmail.com>
2024-12-07 16:15:09 +0200

    Revert "BT: Set system property for Bluetooth Auto On for P23"
    This reverts commit 8a517f4a68d27bf2e13f6a966df9fc7bcbe9a5ce.
    
    Issue: calyxos#2767
    Change-Id: Ib00acfd99d58e6ee3f650f90057334988b6bd34e

--- device-husky.mk
+++ device-husky.mk
@@ -54,10 +54,6 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.se.omapi.ese.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.ese.xml \
 	frameworks/native/data/etc/android.hardware.se.omapi.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.uicc.xml
 
-# Enable Bluetooth AutoOn feature
-PRODUCT_PRODUCT_PROPERTIES += \
-    bluetooth.server.automatic_turn_on=true
-
 # POF
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.bluetooth.finder.supported=true
