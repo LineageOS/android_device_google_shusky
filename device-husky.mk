84f16431deaf048b4cca5a3da97995165a0802bf
Barry <wangbarry@google.com>
2025-01-31 07:13:01 +0000

    Bluetooth: Enable BQR RF Stats event in P23
    Bluetooth: Enable BQR RF Stats event in P23
    
    Enable BQR RF Stats event (bit 8 = 1) at BQR event Mask
       0x100 = 256, default value(295006) + 256 = 295262
    
    Bug: 393496586
    Test: Manual Test
    Flag: EXEMPT system prop update
    Change-Id: Ib8767ba32864717aa31501e763da182a9adbe644

--- device-husky.mk
+++ device-husky.mk
@@ -201,13 +201,13 @@ PRODUCT_PRODUCT_PROPERTIES += \
 # Override BQR mask to enable LE Audio Choppy report, remove BTRT logging
 ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
 PRODUCT_PRODUCT_PROPERTIES += \
-    persist.bluetooth.bqr.event_mask=295006 \
+    persist.bluetooth.bqr.event_mask=295262 \
     persist.bluetooth.bqr.vnd_quality_mask=29 \
     persist.bluetooth.bqr.vnd_trace_mask=0 \
     persist.bluetooth.vendor.btsnoop=true
 else
 PRODUCT_PRODUCT_PROPERTIES += \
-    persist.bluetooth.bqr.event_mask=295006 \
+    persist.bluetooth.bqr.event_mask=295262 \
     persist.bluetooth.bqr.vnd_quality_mask=16 \
     persist.bluetooth.bqr.vnd_trace_mask=0 \
     persist.bluetooth.vendor.btsnoop=false
