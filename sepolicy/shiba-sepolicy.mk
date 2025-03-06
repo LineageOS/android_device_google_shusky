edaa24c826785bab0c7fb9b8183c0d722ce52343
Michael Bestas <mkbestas@gmail.com>
2025-03-06 23:31:19 +0200

    Include missing vibrator sepolicy
    Change-Id: Iac3a36e884dc1747d4fd0a60b418ffbf3779ffdb

--- sepolicy/shiba-sepolicy.mk
+++ sepolicy/shiba-sepolicy.mk
@@ -3,3 +3,6 @@ BOARD_SEPOLICY_DIRS += device/google/shusky/sepolicy/shiba
 
 # unresolved SELinux error log with bug tracking
 BOARD_SEPOLICY_DIRS += device/google/shusky/sepolicy/tracking_denials
+
+BOARD_VENDOR_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/vibrator/common
+BOARD_VENDOR_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/vibrator/cs40l26
