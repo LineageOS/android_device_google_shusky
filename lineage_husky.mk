5e62b21a63dd4457045321ebcae925acb7c120ca
Michael Bestas <mkbestas@gmail.com>
2025-06-14 02:02:07 +0300

    Update fingerprint/build description from BP2A.250605.031.A2
    Change-Id: I8a0bc1927949e81066f0a81058b8f500795b64bf

--- lineage_husky.mk
+++ lineage_husky.mk
@@ -26,8 +26,8 @@ TARGET_SCREEN_HEIGHT := 2992
 TARGET_SCREEN_WIDTH := 1344
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="husky-user 15 BP1A.250505.005.B1 13277630 release-keys" \
-    BuildFingerprint=google/husky/husky:15/BP1A.250505.005.B1/13277630:user/release-keys \
+    BuildDesc="husky-user 16 BP2A.250605.031.A2 13578606 release-keys" \
+    BuildFingerprint=google/husky/husky:16/BP2A.250605.031.A2/13578606:user/release-keys \
     DeviceProduct=$(DEVICE_CODENAME)
 
 $(call inherit-product, $(VENDOR_PATH)/$(DEVICE_CODENAME)-vendor.mk)
--- lineage_shiba.mk
+++ lineage_shiba.mk
@@ -26,8 +26,8 @@ TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="shiba-user 15 BP1A.250505.005.B1 13277630 release-keys" \
-    BuildFingerprint=google/shiba/shiba:15/BP1A.250505.005.B1/13277630:user/release-keys \
+    BuildDesc="shiba-user 16 BP2A.250605.031.A2 13578606 release-keys" \
+    BuildFingerprint=google/shiba/shiba:16/BP2A.250605.031.A2/13578606:user/release-keys \
     DeviceProduct=$(DEVICE_CODENAME)
 
 $(call inherit-product, $(VENDOR_PATH)/$(DEVICE_CODENAME)-vendor.mk)
