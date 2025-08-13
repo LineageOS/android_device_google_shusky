c1cc8dd8c21cf65121a68301750ede2b75677bd0
LuK1337 <priv.luk@gmail.com>
2025-08-13 10:43:50 +0000

    shusky: We don't support udfps touch handler
    This commit fixes broken UDFPS on gs101/gs201/zuma. The breakage is a
    combination of two changes: The A16 UDFPS HAL fallback switching from
    false to true, and the introduction of
    `topic:fingerprint-halHandlesDisplayTouches`.
    
    When HAL sets halHandlesDisplayTouches to true, its onPointerDown() /
    onPointerUp() methods won't be called, which in this case will simply
    break it.
    
    This went unnoticed on Android 15 because the HAL defaulted to false and
    the fps_touch_handler_supported property was not set.
    
    Change-Id: If24475b5f14b7f02b90774be6acf20865285f1ab
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- device-husky.mk
+++ device-husky.mk
@@ -285,6 +285,7 @@ PRODUCT_VENDOR_PROPERTIES += \
 
 PRODUCT_VENDOR_PROPERTIES += \
     persist.vendor.udfps.als_feed_forward_supported=true \
+    persist.vendor.udfps.fps_touch_handler_supported=false \
     persist.vendor.udfps.lhbm_controlled_in_hal_supported=true
 
 # Fingerprint exposure compensation
