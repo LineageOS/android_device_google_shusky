ee4aec7e94a7ba5e13421d0be8a54205db7269be
Michael Bestas <mkbestas@lineageos.org>
2025-11-25 23:30:35 +0200

    shusky: Remove dependency on gs-common makefiles
    Change-Id: I36cbbf9dc0dccce887681af595e856658d46ce76

--- sepolicy/husky-sepolicy.mk
+++ sepolicy/husky-sepolicy.mk
@@ -4,5 +4,15 @@ BOARD_SEPOLICY_DIRS += device/google/shusky/sepolicy/husky
 # unresolved SELinux error log with bug tracking
 BOARD_SEPOLICY_DIRS += device/google/shusky/sepolicy/tracking_denials
 
+BOARD_VENDOR_SEPOLICY_DIRS += device/google/gs-common/bcmbt/sepolicy
+BOARD_VENDOR_SEPOLICY_DIRS += device/google/gs-common/gps/brcm/sepolicy
+BOARD_VENDOR_SEPOLICY_DIRS += device/google/gs-common/touch/gti/ical/sepolicy
+BOARD_VENDOR_SEPOLICY_DIRS += device/google/gs-common/touch/gti/predump_sepolicy
+BOARD_VENDOR_SEPOLICY_DIRS += device/google/gs-common/touch/predump/sepolicy
+BOARD_VENDOR_SEPOLICY_DIRS += device/google/gs-common/touch/stm/predump_sepolicy
+BOARD_VENDOR_SEPOLICY_DIRS += device/google/gs-common/touch/touchinspector/sepolicy
+BOARD_VENDOR_SEPOLICY_DIRS += device/google/gs-common/wireless_charger/sepolicy
+
+BOARD_VENDOR_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/powerstats
 BOARD_VENDOR_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/vibrator/common
 BOARD_VENDOR_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/vibrator/cs40l26
