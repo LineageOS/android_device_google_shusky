87e2e1c1b3cb88fc4ce8dd6dbfa609b87ff4d1ac
Michael Bestas <mkbestas@lineageos.org>
2025-11-25 14:30:41 +0200

    shusky: Cleanup PRODUCT_PACKAGES
    Tag is already included in build/make.
    Iwlan is already included in device/google/zuma.
    Don't explicitly build vendor libs.
    
    Change-Id: I60f1d459acc1c630e61fb3d2f32e63a931456fb6

--- device-husky.mk
+++ device-husky.mk
@@ -51,7 +51,6 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.nfc.ese.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.ese.xml
 
 PRODUCT_PACKAGES += \
-	Tag \
 	android.hardware.nfc-service.st
 
 # SecureElement
@@ -107,11 +106,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
     persist.bluetooth.bqr.vnd_quality_mask=16 \
     persist.bluetooth.bqr.vnd_trace_mask=0 \
     persist.bluetooth.vendor.btsnoop=false
-
-# Spatial Audio
-PRODUCT_PACKAGES += \
-	libspatialaudio \
-	librondo
 
 # Bluetooth Super Wide Band
 PRODUCT_PRODUCT_PROPERTIES += \
@@ -171,8 +165,7 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_PACKAGES += \
 	UwbOverlayHK3 \
 	UwbOverlayHK3Gsi \
-	WifiOverlay2023 \
-	PixelWifiOverlay2023
+	WifiOverlay2023
 
 # Location
 # SDK build system
