342835b6af9c362889f42a6c083308476b155c15
Michael Bestas <mkbestas@lineageos.org>
2025-12-05 09:17:34 +0200

    shusky: Commonize BoardConfig configuration
    Change-Id: I69975c928a0d5bb74dfc9e0e5685d5319d1048b0

--- shiba/BoardConfig.mk
+++ shiba/BoardConfig.mk
@@ -5,30 +5,13 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Enable load module in parallel
-BOARD_BOOTCONFIG += androidboot.load_modules_parallel=true
-
-# The modules which need to be loaded in sequential
-BOARD_KERNEL_CMDLINE += fips140.load_sequential=1
-BOARD_KERNEL_CMDLINE += exynos_drm.load_sequential=1
-BOARD_KERNEL_CMDLINE += g2d.load_sequential=1
-BOARD_KERNEL_CMDLINE += samsung_iommu_v9.load_sequential=1
-
-TARGET_BOARD_INFO_FILE := device/google/shusky/board-info.txt
-TARGET_BOOTLOADER_BOARD_NAME := shiba
+TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
+TARGET_BOOTLOADER_BOARD_NAME := $(DEVICE_CODENAME)
 TARGET_SCREEN_DENSITY := 420
 
-BOARD_KERNEL_CMDLINE += swiotlb=noforce
-BOARD_KERNEL_CMDLINE += disable_dma32=on
-
-include device/google/zuma/BoardConfig-common.mk
+# SEPolicy
 include device/google/shusky/sepolicy/shiba-sepolicy.mk
-include device/google/shusky/wifi/BoardConfig-wifi.mk
 
-# Kernel modules
-BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_BLOCKLIST_FILE := $(DEVICE_PATH)/recovery/modules.blocklist.vendor_kernel_boot
-BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_LOAD_RAW := $(strip $(shell cat $(DEVICE_PATH)/recovery/modules.load.vendor_kernel_boot))
-BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_LOAD += $(BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_LOAD_RAW)
-BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES += $(addprefix $(KERNEL_MODULE_DIR)/, $(notdir $(BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_LOAD_RAW)))
+include $(DEVICE_PATH)/BoardConfigCommon.mk
 
 include $(VENDOR_PATH)/BoardConfigVendor.mk
