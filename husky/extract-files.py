b63b1f9dabd0bb94410fd8fc8e2ffea1e3f9a6dd
Michael Bestas <mkbestas@lineageos.org>
2025-12-05 11:19:34 +0200

    shusky: Replace userdebug checks in scripts
    Change-Id: Ia326a3eff1fad5badf1282850939a90663b6d1c4

--- husky/extract-files.py
+++ husky/extract-files.py
@@ -54,6 +54,11 @@ blob_fixups: blob_fixups_user_type = {
         .patch_file('osaifu-keitai.patch'),
     'vendor/etc/init/init.modem_logging_control.rc': blob_fixup()
         .regex_replace(' && property:ro.debuggable=0', ''),
+    (
+        'vendor/etc/init/init.display_logbuffer.rc',
+        'vendor/etc/init/init.storage.rc',
+    ) : blob_fixup()
+        .regex_replace('ro.build.type=userdebug', 'ro.debuggable=1'),
     'vendor/lib64/libspeechenhancer.so': blob_fixup()
         .clear_symbol_version('AHardwareBuffer_allocate')
         .clear_symbol_version('AHardwareBuffer_describe')
