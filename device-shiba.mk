e24ce3b988b3c9bbe7d3a9f3c7ae012dcbd20661
Michael Bestas <mkbestas@lineageos.org>
2025-12-06 19:54:41 +0200

    shusky: Shuffle & update product properties
    Change-Id: I3818ee42547db84c7377bb8cbfb9ce1c9674d726

--- device-shiba.mk
+++ device-shiba.mk
@@ -54,74 +54,6 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.se.omapi.ese.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.ese.xml \
 	frameworks/native/data/etc/android.hardware.se.omapi.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.se.omapi.uicc.xml
 
-# POF
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.bluetooth.finder.supported=true
-
-# Bluetooth OPUS codec
-PRODUCT_PRODUCT_PROPERTIES += \
-    persist.bluetooth.opus.enabled=true
-
-# Bluetooth AAC VBR
-PRODUCT_PRODUCT_PROPERTIES += \
-    persist.bluetooth.a2dp_aac.vbr_supported=true
-
-# Override BQR mask to enable LE Audio Choppy report, remove BTRT logging
-PRODUCT_PRODUCT_PROPERTIES += \
-    persist.bluetooth.bqr.event_mask=295262 \
-    persist.bluetooth.bqr.vnd_quality_mask=16 \
-    persist.bluetooth.bqr.vnd_trace_mask=0 \
-    persist.bluetooth.vendor.btsnoop=false
-
-# Bluetooth Super Wide Band
-PRODUCT_PRODUCT_PROPERTIES += \
-	bluetooth.hfp.swb.supported=true
-
-# Bluetooth LE Audio
-PRODUCT_PRODUCT_PROPERTIES += \
-	ro.bluetooth.leaudio_switcher.supported?=true \
-	bluetooth.profile.bap.unicast.client.enabled?=true \
-	bluetooth.profile.csip.set_coordinator.enabled?=true \
-	bluetooth.profile.hap.client.enabled?=true \
-	bluetooth.profile.mcp.server.enabled?=true \
-	bluetooth.profile.ccp.server.enabled?=true \
-	bluetooth.profile.vcp.controller.enabled?=true
-
-# Bluetooth LE Audio Broadcast
-PRODUCT_PRODUCT_PROPERTIES += \
-	bluetooth.profile.bap.broadcast.assist.enabled?=true \
-	bluetooth.profile.bap.broadcast.source.enabled?=true
-
-# Bluetooth LE Audio enable hardware offloading
-PRODUCT_PRODUCT_PROPERTIES += \
-	ro.bluetooth.leaudio_offload.supported=true \
-	persist.bluetooth.leaudio_offload.disabled=false
-
-# LE Audio Unicast Allowlist
-PRODUCT_PRODUCT_PROPERTIES += \
-    persist.bluetooth.leaudio.allow_list=SM-R510,WF-1000XM5,SM-R630
-
-# Bluetooth LE Audio CIS handover to SCO
-# Set the property only for the controller couldn't support CIS/SCO simultaneously. More detailed in b/242908683.
-PRODUCT_PRODUCT_PROPERTIES += \
-	persist.bluetooth.leaudio.notify.idle.during.call=true
-
-# Support LE Audio dual mic SWB call
-PRODUCT_PRODUCT_PROPERTIES += \
-    bluetooth.leaudio.dual_bidirection_swb.supported=true
-
-# Disable Bluetooth HAP by default
-PRODUCT_PRODUCT_PROPERTIES += \
-    bluetooth.profile.hap.enabled_by_default=false
-
-# Support LE & Classic concurrent encryption (b/330704060)
-PRODUCT_PRODUCT_PROPERTIES += \
-    bluetooth.ble.allow_enc_with_bredr=true
-
-# Support One-Handed mode
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.support_one_handed_mode=true
-
 # WiFi Overlay
 PRODUCT_PACKAGES += \
 	WifiOverlay2023
@@ -129,15 +61,6 @@ PRODUCT_PACKAGES += \
 # Location
 PRODUCT_PACKAGES += \
     android.hardware.location.gps.prebuilt.xml
-
-# Setup Wizard device-specific settings
-PRODUCT_PRODUCT_PROPERTIES += \
-    setupwizard.feature.enable_quick_start_flow=true \
-
-# Quick Start device-specific settings
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.quick_start.oem_id=00e0 \
-    ro.quick_start.device_id=shiba
 
 # Settings Overlay
 PRODUCT_PACKAGES += \
@@ -150,31 +73,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/window_extensions.mk)
 PRODUCT_SYSTEM_PROPERTIES += \
     persist.settings.large_screen_opt.enabled=false
 
-# Keyboard bottom padding in dp for portrait mode
-PRODUCT_PRODUCT_PROPERTIES += \
-     ro.com.google.ime.kb_pad_port_b=8
-
 PRODUCT_PACKAGES += \
     NfcOverlayShiba
-
-# Set support hide display cutout feature
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.support_hide_display_cutout=true
 
 PRODUCT_PACKAGES += \
     NoCutoutOverlay \
     AvoidAppsInCutoutOverlay
 
 PRODUCT_NO_BIONIC_PAGE_SIZE_MACRO := true
-
-# Bluetooth device id
-# Shiba: 0x410E
-PRODUCT_PRODUCT_PROPERTIES += \
-    bluetooth.device_id.product_id=16654
-
-# Set support for LEA multicodec
-PRODUCT_PRODUCT_PROPERTIES += \
-    bluetooth.core.le_audio.codec_extension_aidl.enabled=true
 
 # ANGLE - Almost Native Graphics Layer Engine
 PRODUCT_PACKAGES += \
