4710cbefe1fbff1f3725d84097468219e4b395f1
Michael Bestas <mkbestas@lineageos.org>
2025-12-06 10:42:09 +0200

    shusky: Move common cmdline to zuma
    Change-Id: If27bea7321b54c5e047931ad3160c04b7b983c4e

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -6,10 +6,6 @@
 
 include device/google/zuma/BoardConfig-common.mk
 
-# Kernel
-BOARD_KERNEL_CMDLINE += swiotlb=noforce
-BOARD_KERNEL_CMDLINE += disable_dma32=on
-
 # Kernel modules
 BOARD_BOOTCONFIG += androidboot.load_modules_parallel=true
 
