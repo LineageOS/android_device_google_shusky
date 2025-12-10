7ea29d45afb9d1598d51daaac0bd2de7c49278a1
Michael Bestas <mkbestas@lineageos.org>
2025-12-10 05:15:16 +0200

    Update fingerprint/build description from BP4A.251205.006
    Change-Id: Ie5906946b02f22870ce87b7fa052cd381d7c4b31

--- lineage_shiba.mk
+++ lineage_shiba.mk
@@ -23,8 +23,8 @@ TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="shiba-user 16 BP3A.251105.015 14339231 release-keys" \
-    BuildFingerprint=google/shiba/shiba:16/BP3A.251105.015/14339231:user/release-keys \
+    BuildDesc="shiba-user 16 BP4A.251205.006 14401865 release-keys" \
+    BuildFingerprint=google/shiba/shiba:16/BP4A.251205.006/14401865:user/release-keys \
     DeviceProduct=$(DEVICE_CODENAME)
 
 $(call inherit-product, $(VENDOR_PATH)/$(DEVICE_CODENAME)-vendor.mk)
