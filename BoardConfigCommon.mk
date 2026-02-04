5113e593cf94dc3291de8dcb7c70ac5802a9f935
Nolen Johnson <johnsonnolen@gmail.com>
2026-02-03 21:35:59 -0500

    SPL: BP4A.260205.001
    Change-Id: Ia886e66988fc6b745ba1568a708e57a59fb4676e

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -5,7 +5,7 @@
 #
 
 # Security - must be defined before including BoardConfig-common.mk
-BOOT_SECURITY_PATCH := 2026-01-05
+BOOT_SECURITY_PATCH := 2026-02-05
 VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
 
 include device/google/zuma/BoardConfig-common.mk
