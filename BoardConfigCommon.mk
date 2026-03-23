a760c2dfe901c3cb54cf28dd1f6904d703008843
Michael Bestas <mkbestas@lineageos.org>
2026-03-24 01:28:32 +0200

    shusky: Update bootconfig/cmdline from BP4A
    Change-Id: I4a8888a49560d0d295439b40448f41be2e33f269

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -11,13 +11,6 @@ VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
 include device/google/zuma/BoardConfig-common.mk
 
 # Kernel modules
-BOARD_BOOTCONFIG += androidboot.load_modules_parallel=true
-
-BOARD_KERNEL_CMDLINE += fips140.load_sequential=1
-BOARD_KERNEL_CMDLINE += exynos_drm.load_sequential=1
-BOARD_KERNEL_CMDLINE += g2d.load_sequential=1
-BOARD_KERNEL_CMDLINE += samsung_iommu_v9.load_sequential=1
-
 BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_BLOCKLIST_FILE := $(DEVICE_PATH)/recovery/modules.blocklist.vendor_kernel_boot
 BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_LOAD_RAW := $(strip $(shell cat $(DEVICE_PATH)/recovery/modules.load.vendor_kernel_boot))
 BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_LOAD += $(BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_LOAD_RAW)
