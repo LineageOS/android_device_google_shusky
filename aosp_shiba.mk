cff320d70c2efe90492ed5303ec83165d43d2409
Michael Bestas <mkbestas@lineageos.org>
2026-06-24 10:32:51 +0300

    shusky: Update properties from CP2A.260605.012
    Change-Id: Id3b703fb13a9418d3e9b1c61efab6d5154a66f6a

--- aosp_shiba.mk
+++ aosp_shiba.mk
@@ -8,6 +8,12 @@ $(call inherit-product, device/google/shusky/device-shiba.mk)
 
 PRODUCT_NAME := aosp_shiba
 PRODUCT_DEVICE := shiba
-PRODUCT_MODEL := AOSP on shiba
-PRODUCT_BRAND := Android
+PRODUCT_MODEL := Pixel 8
+PRODUCT_BRAND := google
 PRODUCT_MANUFACTURER := Google
+
+PRODUCT_NAME_FOR_ATTESTATION := shiba
+PRODUCT_DEVICE_FOR_ATTESTATION := shiba
+PRODUCT_MODEL_FOR_ATTESTATION := Pixel 8
+PRODUCT_BRAND_FOR_ATTESTATION := google
+PRODUCT_MANUFACTURER_FOR_ATTESTATION := Google
