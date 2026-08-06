7c8e4b1aaed5620ebf895311607826356f969cbc
Michael Bestas <mkbestas@lineageos.org>
2026-08-06 04:58:41 +0300

    shusky: Update to CP2A.260805.005
    Change-Id: Ibd8467a856eeb9c8da1751d1b908e6ae4c04310d

--- lineage_husky.mk
+++ lineage_husky.mk
@@ -24,9 +24,9 @@ TARGET_SCREEN_HEIGHT := 2992
 TARGET_SCREEN_WIDTH := 1344
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="husky-user 17 CP2A.260705.006 15641320 release-keys" \
-    BuildFingerprint=google/husky/husky:17/CP2A.260705.006/15641320:user/release-keys \
-    BuildSystemFingerprint=google/generic_system_google/generic:17/CP2A.260705.006/15641320:user/release-keys \
+    BuildDesc="husky-user 17 CP2A.260805.005 15828068 release-keys" \
+    BuildFingerprint=google/husky/husky:17/CP2A.260805.005/15828068:user/release-keys \
+    BuildSystemFingerprint=google/generic_system_google/generic:17/CP2A.260805.005/15828068:user/release-keys \
     DeviceProduct=$(DEVICE_CODENAME)
 
 $(call inherit-product, $(VENDOR_PATH)/$(DEVICE_CODENAME)-vendor.mk)
